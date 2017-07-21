c0ff721f7473aae12e4be3942d422a5aa94fdd86
eugengev <eugengev@gmail.com>
2017-07-21 16:21:07 +0300

    first commit

--- js/common.js
+++ js/common.js
@@ -68,26 +68,48 @@ $(function() {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                    slidesToScroll: 3
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    centerMode: false,
+                    variableWidth: false,
+                    arrows: false
                 }
             }
         ]
     });
 
 });
+
+var map;
+function initMap() {
+    var myLatLng = {lat: 49.4225715, lng: 26.9763337};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        scrollwheel: false,
+        center:  {lat: 49.4225715, lng: 26.9763337},  // Brooklyn.
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP]
+        }
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'ТОчка на карте'
+    });
+}
+
